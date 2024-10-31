62ffac19e9ded886c0762b5cde6d755da8743c2f
hamarm2 <hamarmwork@gmail.com>
2024-10-31 23:37:41 +0100

    Začátek s MQTT

--- esp_kody/main.py
+++ esp_kody/main.py
@@ -7,6 +7,13 @@ ledIn = Pin(2, Pin.OUT)
 
 sta_if = network.WLAN(network.STA_IF)
 
+BROKER_IP = '147.228.124.47'
+BROKER_PORT = 1883
+BROKER_UNAME = 'student'
+BROKER_PASSWD = 'pivotecepomqtt'
+TOPIC = 'ite/yellow'
+SLEEP_TIME = 2
+
 if not sta_if.isconnected():
     print('connecting to network...')
     sta_if.active(True)
@@ -15,6 +22,8 @@ if not sta_if.isconnected():
     while not sta_if.isconnected():
         pass
 print('network config:', sta_if.ifconfig())
+
+MQclient = umqtt.MQTTClient("yellow_esp", BROKER_IP, BROKER_PORT, BROKER_UNAME, BROKER_PASSWD)
 
 while(True):
     ledIn.on()
