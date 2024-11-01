5c6955064d4f7dbd129d0e9b579a31d6333e482c
hamarm2 <hamarmwork@gmail.com>
2024-11-01 13:48:56 +0100

    MQTT test.

--- mpfshell/main.py
+++ mpfshell/main.py
@@ -1,10 +1,18 @@
 from machine import Pin
 import network
 import time
+import umqtt
 
 ledIn = Pin(2, Pin.OUT)
 
 sta_if = network.WLAN(network.STA_IF)
+
+BROKER_IP = '147.228.124.47'
+BROKER_PORT = 1883
+BROKER_UNAME = 'student'
+BROKER_PASSWD = 'pivotecepomqtt'
+TOPIC = 'ite/yellow'
+SLEEP_TIME = 2
 
 if not sta_if.isconnected():
     print('connecting to network...')
@@ -15,8 +23,13 @@ if not sta_if.isconnected():
         pass
 print('network config:', sta_if.ifconfig())
 
+MQclient = umqtt.MQTTClient("yellow_esp", BROKER_IP, BROKER_PORT, BROKER_UNAME, BROKER_PASSWD)
+MQclient.connect()
+
 while(True):
     ledIn.on()
     time.sleep_ms(500)
+    MQclient.publish(TOPIC, "Query", qos=1)
     ledIn.off()
     time.sleep_ms(500)
+    
