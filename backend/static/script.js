74dd1cf66294807374432fdb27d9d6181a376b17
Samuel Kokoska <sam.kokoska@hotmail.cz>
2024-11-10 17:49:52 +0100

    Zbavil jsem se innerHTML. Přidání hezčího datumu a času.

--- backend/static/script.js
+++ backend/static/script.js
@@ -1,5 +1,3 @@
-
-span.innerHTML = "nothing";
 function onBodyLoad() {
     ws = new WebSocket('ws://localhost:8881/websocket')     // ws is a global variable (index.html)
     ws.onopen = onSocketOpen
@@ -21,47 +19,63 @@ function onSocketMessage(message) {
         data = message.data
     }
     let sensorDataArr = data.sensor_data
-    sensorDataArr.forEach((sensor) => {
-        console.log(`Team Name: ${sensor.team_name}`);
-        console.log(`ID: ${sensor.id}`);
-        console.log(`Team ID: ${sensor.team_id}`);
-        console.log(`Timestamp: ${sensor.timestamp}`);
-        console.log(`Temperature: ${sensor.temperature}`);
-        console.log(`Humidity: ${sensor.humidity}`);
-        console.log(`Illumination: ${sensor.illumination}`);
-        console.log("---------------");
-        updateTeamData(sensorDataArr)
-    });
+    // sensorDataArr.forEach((sensor) => {
+        // console.log(`Team Name: ${sensor.team_name}`);
+        // console.log(`ID: ${sensor.id}`);
+        // console.log(`Team ID: ${sensor.team_id}`);
+        // console.log(`Timestamp: ${sensor.timestamp}`);
+        // console.log(`Temperature: ${sensor.temperature}`);
+        // console.log(`Humidity: ${sensor.humidity}`);
+        // console.log(`Illumination: ${sensor.illumination}`);
+        // console.log("---------------"); 
+    // });
+    updateTeamData(sensorDataArr)
 }
 
 function updateTeamData(sensorDataArray) {
     // Iterate through each sensor data and update the corresponding element
     sensorDataArray.forEach((sensor) => {
         let team_name = sensor.team_name;
-
+        let dateAndTime;
+        
         // Dynamically create the element IDs based on the team_id
-        let timestampElement = document.getElementById(`team-${team_name}-timestamp`);
+        let dateElement = document.getElementById(`team-${team_name}-date`);
+        let timeElement = document.getElementById(`team-${team_name}-time`);
+
         let tempElement = document.getElementById(`team-${team_name}-temp`);
         let humidityElement = document.getElementById(`team-${team_name}-humidity`);
         let illuminationElement = document.getElementById(`team-${team_name}-illumination`);
-
+        if (sensor.timestamp){
+            dateAndTime = extractDateAndTime( sensor.timestamp);
+        }
         // If the element exists, update its innerHTML
-        if (timestampElement) {
-            timestampElement.innerHTML = sensor.timestamp;
+        
+        if (dateElement) {
+            dateElement.textContent = dateAndTime.date.toDateString() ;
+        }
+        if (timeElement) {
+            timeElement.textContent = dateAndTime.time;
         }
         if (tempElement) {
-            tempElement.innerHTML = sensor.temperature;
+            tempElement.textContent = sensor.temperature;
         }
         if (humidityElement) {
-            humidityElement.innerHTML = sensor.humidity;
+            humidityElement.textContent = sensor.humidity;
         }
         if (illuminationElement) {
-            illuminationElement.innerHTML = sensor.illumination;
+            illuminationElement.textContent = sensor.illumination;
         }
+        
     });
 }
 
-
+function extractDateAndTime(dateObj){
+    dateAndTime = String(dateObj);
+    let [dateStr, time] = dateAndTime.split("T");
+    var parts = dateStr.split('-');
+    var date = new Date(parts[0], parts[1] - 1, parts[2]); 
+    return {date, time}
+}
 
 function onSocketClose() {
     console.log("WS client: Websocket closed.")
