135d616d1a459557c8b2527fd5a9d0cfd3280e78
Samuel Kokoska <sam.kokoska@hotmail.cz>
2024-11-10 12:08:45 +0100

    Zobrazení dat na frontendu.

--- backend/static/script.js
+++ backend/static/script.js
@@ -20,10 +20,48 @@ function onSocketMessage(message) {
     } catch(e) {
         data = message.data
     }
-    console.log(message.data)
-        
-    
+    let sensorDataArr = data.sensor_data
+    sensorDataArr.forEach((sensor) => {
+        console.log(`Team Name: ${sensor.team_name}`);
+        console.log(`ID: ${sensor.id}`);
+        console.log(`Team ID: ${sensor.team_id}`);
+        console.log(`Timestamp: ${sensor.timestamp}`);
+        console.log(`Temperature: ${sensor.temperature}`);
+        console.log(`Humidity: ${sensor.humidity}`);
+        console.log(`Illumination: ${sensor.illumination}`);
+        console.log("---------------");
+        updateTeamData(sensorDataArr)
+    });
 }
+
+function updateTeamData(sensorDataArray) {
+    // Iterate through each sensor data and update the corresponding element
+    sensorDataArray.forEach((sensor) => {
+        let team_name = sensor.team_name;
+
+        // Dynamically create the element IDs based on the team_id
+        let timestampElement = document.getElementById(`team-${team_name}-timestamp`);
+        let tempElement = document.getElementById(`team-${team_name}-temp`);
+        let humidityElement = document.getElementById(`team-${team_name}-humidity`);
+        let illuminationElement = document.getElementById(`team-${team_name}-illumination`);
+
+        // If the element exists, update its innerHTML
+        if (timestampElement) {
+            timestampElement.innerHTML = sensor.timestamp;
+        }
+        if (tempElement) {
+            tempElement.innerHTML = sensor.temperature;
+        }
+        if (humidityElement) {
+            humidityElement.innerHTML = sensor.humidity;
+        }
+        if (illuminationElement) {
+            illuminationElement.innerHTML = sensor.illumination;
+        }
+    });
+}
+
+
 
 function onSocketClose() {
     console.log("WS client: Websocket closed.")
