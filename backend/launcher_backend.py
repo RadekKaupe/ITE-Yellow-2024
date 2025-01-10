140d33205a048ea8c7eda59c0087106b0bd8761d
hamarm2 <hamarmwork@gmail.com>
2025-01-10 12:59:25 +0100

    Launchers now redirect stdout to .txt files.

--- backend/launcher_backend.py
+++ backend/launcher_backend.py
@@ -1,11 +1,15 @@
 import os
 import time
+from contextlib import redirect_stdout
 
 script = os.path.join(os.path.dirname(__file__), "backend.py")
+path = os.path.join(os.path.dirname(__file__), 'log_backend.txt')
 while(True):
-    try:
-        exec(open(script).read())
-    except:
-        print("backend crashed")
-        time.sleep(10)
+    with open(path, 'a') as file:
+        with redirect_stdout(file):
+            try:
+                exec(open(script).read())
+            except:
+                print("backend crashed")
+                time.sleep(10)
         
--- subscriber/launcher_subscriber.py
+++ subscriber/launcher_subscriber.py
@@ -1,10 +1,14 @@
 import os
 import time
+from contextlib import redirect_stdout
 
 script = os.path.join(os.path.dirname(__file__), "subscriber_vm.py")
+path = os.path.join(os.path.dirname(__file__), 'log_subscriber.txt')
 while(True):
-    try:
-        exec(open(script).read())
-    except:
-        print("subscriber crashed")
-        time.sleep(10)
+    with open(path, 'a') as file:
+        with redirect_stdout(file):
+            try:
+                exec(open(script).read())
+            except:
+                print("subscriber crashed")
+                time.sleep(10)
